c9bc80e9d034b1a54d56fcfeb888b3af50f437bb
Tomas Panyokaew <tomaspanyokaew@hotmail.com>
2026-01-24 18:49:26 +0100

    configure EJS as template engine

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
+const PORT = 5080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// EJS (SSR templates)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Static dist
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(5080);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
